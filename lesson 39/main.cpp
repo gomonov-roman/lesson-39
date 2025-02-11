bd64b09af007e6a9dc5769441d359909959cd880
roman ivanov <gomonov1987@mail.ru>
2025-02-11 17:37:28 +0300

    add inin and convert functions

---
+++ lesson 39/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+	int number;
+		cout << "Input   your number: ";
+		cin >> number;
+ for (int i = 2; i <= number; i += 2) {
+	cout << i << " ";
+}
+
+
+	return 0;
+}
+
+
+
+
+
+
+
---
+++ lesson 39/util.cpp
@@ -0,0 +1,9 @@
+#include util.h
+
+string convert(int* array, int  size);
+void init(int* array, int  size, int a, int b){
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = 
+	}
+}
